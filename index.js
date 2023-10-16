cdcfdf0709c1bda46380a9876b8d56dd1924ac13
clayg <claytondguerrero@gmail.com>
2023-10-15 20:31:51 -0500

    created seperate files for each shape, and created shapeSelectHandler function

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const inquirer = require("inquirer");
-const { Triangle, Circle, Square } = require("./lib/shapes");
-const { writeFile } = require('fs').promises
+const Triangle = require('./lib/tri')
+const Circle = require('./lib/circ')
+const Square = require('./lib/shapes')
+const { writeFile } = require("fs").promises;
 
 class Svg {
-  constructor(text, textColor, shape) {
-    this.text = text;
-    this.textColor = textColor;
-    this.shape = shape;
-
+  constructor() {
+    this.text = '';
+    this.shape = '';
   }
   render() {
-    return `<svg
-      version="1.1"
-      width="300"
-      height="200"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      ${this.shape}
-    </svg>;`;
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shape}</svg>`
   }
   setText(text, textColor) {
-
+    this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
   }
   setShape(shape) {
     this.shape = shape.render()
@@ -54,21 +47,40 @@ const questions = [
 ];
 
 function writeSVGFile(name, data) {
-  const newData = JSON.stringify(data)
-  writeFile(name, newData)
+  const newData = JSON.stringify(data);
+  console.log(newData)
+  writeFile(name, newData);
 }
 
 function generateLogo(answers) {
-  const newAns = JSON.parse(answers)
-  console.log(newAns.text, newAns.textColor, newAns.shape);
-  const logo = new Svg(newAns.text, newAns.textColor, newAns.shape)
-  return logo
+  const logo = new Svg(answers.text, answers.textColor, answers.shape, answers.shapeColor);
+  logo.setText(answers.text, answers.textColor)
+  logo.setShape(shapeSelectHandler(answers.shape,answers.shapeColor))
+  return logo.render()
+}
+
+function shapeSelectHandler(shape,color) {
+  let newShape
+  if (shape === 'Triangle') {
+    newShape = new Triangle()
+    newShape.setColor(color)
+    return newShape
+  } else if (shape === 'Circle') {
+    newShape = new Circle
+    newShape.setColor(color)
+    return newShape
+  } else {
+    newShape = new Square()
+    newShape.setColor(color)
+    return newShape
+  }
 }
 
 function init() {
   console.log("started");
-  inquirer.prompt(questions)
-    .then((res) => writeSVGFile('logo.svg', generateLogo(JSON.stringify(res))))
+  inquirer
+    .prompt(questions)
+    .then((res) => writeSVGFile("logo.svg", generateLogo(res)))
     .then(() => console.log("finished"));
 }
 
