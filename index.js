caa790bfa8b5141dc6e7f7391b2143fb2842cecf
clayg <claytondguerrero@gmail.com>
2023-10-17 15:15:03 -0500

    fixed square text render issue

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const Circle = require('./lib/circ')
 const Square = require('./lib/sqre')
 const { writeFile } = require("fs").promises;
 
+
 class Svg {
   constructor() {
     this.text = '';
     this.shape = '';
   }
   render() {
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shape}${this.text}</svg>`
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg" >${this.shape}${this.text}</svg>`
   }
   setText(text, textColor) {
     this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
